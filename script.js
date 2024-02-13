ecbc12b2a76c4731fa4609d5a375afa7502f1aaa
zahid072 <jahidhasan19368@gmail.com>
2024-02-13 21:29:19 +0600

    play ground added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const playBtn = document.getElementById('playBtn');
+const displayInput = document.getElementById('displayInput');
+
+displayInput.addEventListener("input", function() {
+    // Convert the input value to uppercase and assign it back to the input
+    displayInput.value = displayInput.value.toUpperCase();
+});
