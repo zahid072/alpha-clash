613401932d55e9eb5f33f1caf72786e9cb5b929c
zahid072 <jahidhasan19368@gmail.com>
2024-02-13 22:00:55 +0600

    page navigation added

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const playBtn = document.getElementById('playBtn');
-const displayInput = document.getElementById('displayInput');
+const displayInput = document.getElementById("displayInput");
+displayInput.addEventListener("input", function () {
+  // Convert the input value to uppercase and assign it back to the input
+  displayInput.value = displayInput.value.toUpperCase();
+});
 
-displayInput.addEventListener("input", function() {
-    // Convert the input value to uppercase and assign it back to the input
-    displayInput.value = displayInput.value.toUpperCase();
+// main code
+
+const playBtn = document.getElementById("playBtn");
+const playBtn2 = document.getElementById("playBtn2");
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById("section3");
+
+playBtn.addEventListener("click", function () {
+  section1.style.display = "none";
+  section2.style.display = "block";
+});
+playBtn2.addEventListener("click", function () {
+  section1.style.display = "none";
+  section3.style.display = "none";
+  section2.style.display = "block";
 });
